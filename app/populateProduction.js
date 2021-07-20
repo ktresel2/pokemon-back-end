e3ea0c81f65fd27cb8eff0264d990acef25f86ac
ktresel2 <ktreseler2@gmail.com>
2021-07-20 15:58:04 -0400

    Merge

--- app/populateProduction.js
+++ app/populateProduction.js
@@ -10,9 +10,12 @@ const populate = (num) => {
       pokemon.pokeId = res.data.id
       pokemon.image = res.data.sprites.front_shiny
       pokemon.type = res.data.types[0].type.name
+<<<<<<< HEAD
       pokemon.moves = res.data.moves.map(move => move.move.name).slice(0, 3)
       pokemon.weight = res.data.weight
       pokemon.height = res.data.height
+=======
+>>>>>>> c4e5d3d (Add file for populating production db)
       return pokemon
     })
     .then(poke => axios({ method: 'POST', url: 'https://intense-sierra-55545.herokuapp.com/pokemon', data: { poke } }))
@@ -20,7 +23,11 @@ const populate = (num) => {
 }
 
 const getEmAll = () => {
+<<<<<<< HEAD
   for (let i = 1; i < 2; i++) {
+=======
+  for (let i = 2; i < 152; i++) {
+>>>>>>> c4e5d3d (Add file for populating production db)
     populate(i)
   }
 }
