6ba1f3ebf5f5989333848f4a83f380dc20c6719a
Kevin Treseler <kevin.t.developer@gmail.com>
2023-09-16 05:49:43 -0400

    Update schema

--- app/populate.js
+++ app/populate.js
@@ -3,20 +3,26 @@ const axios = require('axios')
 const populate = (num) => {
   let pokemon = {}
   return axios({
-    url: `https://pokeapi.co/api/v2/pokemon/${num}`
+    url: `https://pokeapi.co/api/v2/pokemon/${num}`,
   })
-    .then(res => {
-      pokemon.name = res.data.name
-      pokemon.pokeId = res.data.id
-      pokemon.image = res.data.sprites.front_shiny
-      pokemon.type = res.data.types[0].type.name
-      pokemon.moves = res.data.moves.map(move => move.move.name).slice(0, 3)
-      pokemon.weight = res.data.weight
-      pokemon.height = res.data.height
-      return pokemon
+    .then((res) => {
+      pokemon.name = res.data.name;
+      pokemon.pokeId = res.data.id;
+      pokemon.image = res.data.sprites.front_shiny;
+      pokemon.type = res.data.types[0].type.name;
+      pokemon.moves = res.data.moves.map((move) => move.move.name).slice(0, 3);
+      pokemon.weight = res.data.weight;
+      pokemon.height = res.data.height;
+      return pokemon;
     })
-    .then(poke => axios({ method: 'POST', url: 'http://localhost:4741/pokemon', data: { poke } }))
-    .catch(console.error)
+    .then((poke) =>
+      axios({
+        method: "POST",
+        url: "https://pokemon-squad.onrender.com/pokemon",
+        data: { poke },
+      })
+    )
+    .catch(console.error);
 }
 
 const getEmAll = () => {
